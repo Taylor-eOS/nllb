d86ed7bb0c05f4ca0489d404843a5b0b5699d70d
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-02-24 00:54:16 +0100

    Get summary

--- llm.py
+++ llm.py
@@ -3,42 +3,50 @@ import torch
 import sentencepiece
 import settings
 
-MODEL_NAME = "stabilityai/stablelm-zephyr-3b"
+MODEL_NAME = "Qwen/Qwen2.5-3B-Instruct"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.bfloat16).to(device)
 TABLE = {'eng_Latn': 'English', 'deu_Latn': 'German', 'spa_Latn': 'Spanish', 'jpn_Jpan': 'Japanese', 'fra_Latn': 'French', 'fin_Latn': 'Finnish', 'dan_Latn': 'Danish'} #Add as needed
 
 def get_advice(text, source=settings.SOURCE_LANG, target=settings.TARGET_LANG):
-    #prompt = f"Translate from {TABLE[source]} to {TABLE[target]} without other comments (for use in programmatic translation): `{text}`. Translation:"
-    prompt = f"Translate the {TABLE[source]} sentence \"`{text}`\" into {TABLE[target]}, without other comments (for use in programmatic derival). {TABLE[target]} translation: "
-    #prompt = f"Translate from {TABLE[source]} to {TABLE[target]} without any other comment, as it is used in programmatic translation: `{text}`. This is a suggested translation, correct it: '{suggestion}'. {TABLE[target]} translation: "
+    prompt = f"Write this {TABLE[source]} sentence translated to {TABLE[target]}:  \"`{text}`\". (Only write the sentence itself, no other comment.) Translation: "
     print(prompt)
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
     output_tokens = model.generate(
-        inputs.input_ids, max_new_tokens=100, temperature=0.2, top_p=0.9, do_sample=False)
+        inputs.input_ids, max_new_tokens=300, temperature=0.2, top_p=0.9, do_sample=False)
     result = tokenizer.decode(output_tokens[0], skip_special_tokens=True).strip()
     if "Translation:" in result:
         result = result.split("Translation:")[-1].strip()
     elif text in result:
         result = result.replace(text, "").strip()
-    result = result.replace('"', '')
-    result = result.replace('`', '')
-    result = result.replace('\n', ' ')
-    result = result.replace('..', '.')
-    result = result.replace('\'', '')
-    return result
+    return replace_characters(result)
 
-"""
-def get_advice_dolphin(text, source=settings.SOURCE_LANG, target=settings.TARGET_LANG):
-    prompt = f"Translate from {TABLE[source]} to {TABLE[target]} without any other comment, for use in programmatic translation: `{text}`. Translation:"
-    inputs = tokenizer(prompt, return_tensors="pt", add_special_tokens=True).to(device)
-    output_tokens = model.generate(inputs.input_ids, max_new_tokens=100, temperature=0.2, top_p=0.9, do_sample=False)
+def get_summary(text):
+    prompt = f"Rewrite this sentence concisely: \"`{text}`\". (Write no other comment.) Rewriting: "
+    inputs = tokenizer(prompt, return_tensors="pt").to(device)
+    output_tokens = model.generate(
+        inputs.input_ids, max_new_tokens=150, temperature=0.2, top_p=0.9, do_sample=False)
     result = tokenizer.decode(output_tokens[0], skip_special_tokens=True).strip()
-    if "Translation:" in result:
-        result = result.split("Translation:")[-1].strip()
-    return result.replace('"', '')
-"""
+    if "Rewriting:" in result:
+        result = result.split("Rewriting:")[-1].strip()
+    elif text in result:
+        result = result.replace(text, "").strip()
+    result = result.replace('.', '')
+    return replace_characters(result)
+
+def replace_characters(sentence):
+    #print(f"Original sentence: {sentence}")
+    print("")
+    sentence = sentence.replace('#', '')
+    sentence = sentence.replace('\n', ' ')
+    sentence = sentence.replace('"', '')
+    sentence = sentence.replace('```', '')
+    sentence = sentence.replace('`', '')
+    sentence = sentence.replace('...', '')
+    sentence = sentence.replace('..', '.')
+    sentence = sentence.replace('\'', '')
+    return sentence.strip()
 
 def remove_ending(string):
     if string.endswith("_Latn"):
@@ -46,6 +54,7 @@ def remove_ending(string):
     return string
 
 if __name__ == "__main__":
-    translation_string = get_advice("I am a cat", "Ich sein Katze", "eng_Latn", "deu_Latn")
-    print(translation_string)
+    print(f"---{get_advice("I am a cat!", "eng_Latn", "deu_Latn")}---")
+    print(f"---{get_summary("I am a fairly large feline, of that I am sure.")}---")
+    print(f"---{get_summary("I have all the attributes that would make for a great husband, if you were to visit my farm, you would be convinced of that.")}---")
 
